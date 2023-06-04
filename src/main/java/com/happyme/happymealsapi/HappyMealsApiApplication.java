5988328888302c8fcda8844e20098f5005a31640
tubtim13 <tubtim.kuph@gmail.com>
2023-06-04 16:14:25 +0700

    added project

---
+++ src/main/java/com/happyme/happymealsapi/HappyMealsApiApplication.java
@@ -0,0 +1,13 @@
+package com.happyme.happymealsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMealsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMealsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyme/happymealsapi/HappyMealsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyme.happymealsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMealsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
